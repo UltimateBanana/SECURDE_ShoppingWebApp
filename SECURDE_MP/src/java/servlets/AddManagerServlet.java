3245901bad9036b187bab6de60ce7b57a3135a2e
Phillip <phillip_coquilla@dlsu.edu.ph>
2016-08-12 15:36:52 +0800

    Add Manager

--- SECURDE_MP/src/java/servlets/AddManagerServlet.java
+++ SECURDE_MP/src/java/servlets/AddManagerServlet.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Phil
- */
 public class AddManagerServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
